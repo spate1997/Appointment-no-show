3951284e110ddba957126efb81ca08bc30cc27b4
Sheel <spate022@odu.edu>
2024-07-13 15:22:11 -0400

    start

---
+++ appointment_prediction/predictions/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+
+class Patient(models.Model):
+    name = models.CharField(max_length=100)
+    gender = models.CharField(max_length=10)
+    age = models.IntegerField()
+    neighborhood = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+class Appointment(models.Model):
+    patient = models.ForeignKey(Patient, on_delete=models.CASCADE)
+    appointment_date = models.DateField()
+    scheduled_date = models.DateField()
+    no_show = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f"{self.patient.name} - {self.appointment_date}"
